6cae0d68d661d7cc7e1b4e8b791855cacbaebbb6
Phat Tran <PHAT@Phattv.local>
2018-04-08 00:07:45 +0700

    Remove prettier, use Webstorm prettier with .prettierrc

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require('express'),
-  logger = require('morgan'),
-  bodyParser = require('body-parser'),
-  http = require('http'),
-  compression = require('compression');
+const express = require("express"),
+  logger = require("morgan"),
+  bodyParser = require("body-parser"),
+  http = require("http"),
+  compression = require("compression");
 
 const port = 8001;
 
 // Set up the express app
 const app = express();
-app.enable('trust proxy');
+app.enable("trust proxy");
 
 // Log requests to the console.
-app.use(logger('dev'));
+app.use(logger("dev"));
 
 // Parse incoming requests data (https://github.com/expressjs/body-parser)
 app.use(bodyParser.json());
@@ -21,19 +21,19 @@ app.use(compression());
 
 // Enable CORS for admin-on-rest
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Expose-Headers', 'X-Total-Count');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-  res.header('X-Total-Count', '10');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Expose-Headers", "X-Total-Count");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header("X-Total-Count", "10");
 
   next();
 });
 
-require('./routes')(app);
+require("./routes")(app);
 
 http.createServer(app).listen(port, () => {
-  console.log('environment:', process.env.NODE_ENV);
-  console.log('app is listening on port ' + port);
+  console.log("environment:", process.env.NODE_ENV);
+  console.log("app is listening on port " + port);
 });
